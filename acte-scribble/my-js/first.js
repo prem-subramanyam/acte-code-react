69a317cf163ee010422dc2f7ea80bd06faee1e8a
Test <testuser@testdom.com>
2023-12-17 13:50:56 +0530

    practice commit

---
+++ acte-scribble/my-js/first.js
@@ -0,0 +1,27 @@
+// let array = [1, 2, 3, 4, 5];
+
+// for(let i = array.length-1; i>=0; i-- ) {
+//     console.log(array[i]);
+// }
+
+// Six variables are give as per the following details:
+// a=5
+// b=7
+// c=90
+// d=60
+// e=20
+// f=10
+// Find the value of equation 2x+5y+10*z, where:
+// x= a raised to the power of b.
+// y= sum of c and e.
+// z= product of d and f.
+
+a=5
+b=7
+c=90
+d=60
+e=20
+f=10
+
+let sum = 2*(a**b) + 5*(c+e) + 10*(d*f);
+console.log(sum); 
