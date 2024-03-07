94414a16e7627b84f36d8750d679058c779ce857
Prem <testuser@testdom.com>
2024-03-07 17:13:25 +0530

    higher order and layout practice

--- acte-scribble/scrap-book/first-js.js
+++ acte-scribble/scrap-book/first-js.js
@@ -16,12 +16,24 @@
 // y= sum of c and e.
 // z= product of d and f.
 
-a=5
-b=7
-c=90
-d=60
-e=20
-f=10
+// a=5
+// b=7
+// c=90
+// d=60
+// e=20
+// f=10
 
-let sum = 2*(a**b) + 5*(c+e) + 10*(d*f);
-console.log(sum); 
+// let sum = 2*(a**b) + 5*(c+e) + 10*(d*f);
+// console.log(sum); 
+
+// let num = 1234567890123456789012n;
+// let result = num * BigInt(1000);
+// console.log(result + " " + typeof(result));
+
+let nums = [1,2,3,4,5,6,7,8,9,10];
+
+odd = nums.filter(eachNum => {
+    return (eachNum % 2) != 0;
+});
+
+console.log(odd);
